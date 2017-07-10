eed6f1974690f8a20972c65fa8df56079b3c7b99
tomtrueplus <tom@trueplus.vn>
2017-07-10 16:48:31 +0700

    update model.js

--- sort_pos_product/static/src/js/models.js
+++ sort_pos_product/static/src/js/models.js
@@ -1,15 +1,10 @@
-odoo.define('sort_pos_product.models', function (require) {
+odoo.define('sort_pos_product.models', function(require) {
 "use strict";
 
 var models = require('point_of_sale.models');
-var _super_posmodel = models.PosModel.prototype;
 
-var exports = {};
-
-exports.PosModel = models.Model.extend({
-	models: [
-	{
-        model:  'product.product',
+models.load_models({
+	model:  'product.product',
         fields: ['display_name', 'list_price','price','pos_categ_id', 'taxes_id', 'barcode', 'default_code', 
                  'to_weight', 'uom_id', 'description_sale', 'description',
                  'product_tmpl_id','tracking', 'count_sell'],
@@ -19,9 +14,5 @@ exports.PosModel = models.Model.extend({
         loaded: function(self, products){
             self.db.add_products(products);
         },
-    }
-	],
 });
-
-return exports;
 });
