dfc34b50077908db8dd2ed99ba14efc7844bbad5
tomtrueplus <tom@trueplus.vn>
2017-07-12 13:04:02 +0700

    Fix previours commit

--- sort_pos_product/static/src/js/models.js
+++ sort_pos_product/static/src/js/models.js
@@ -8,11 +8,16 @@ models.load_models({
         fields: ['display_name', 'list_price','price','pos_categ_id', 'taxes_id', 'barcode', 'default_code', 
                  'to_weight', 'uom_id', 'description_sale', 'description',
                  'product_tmpl_id','tracking', 'count_sell', 'quantity'],
-        order:  ['count_sell','sequence','default_code','name'],
+        order:  [ 'sequence','default_code','name'],
         domain: [['sale_ok','=',true],['available_in_pos','=',true]],
         context: function(self){ return { pricelist: self.pricelist.id, display_default_code: false }; },
         loaded: function(self, products){
+        	products.sort(function (a,b) {
+        		return (a.count_sell > b.count_sell) ? -1 : ((b.count_sell > a.count_sell) ? 1 : 0);
+        	});
+        	console.log(products);
             self.db.add_products(products);
+
         },
 });
 });
