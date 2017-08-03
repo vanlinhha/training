c60821c0a42e0526c63e35e141e331ab25103b00
XMars <mars@trueplus.vn>
2017-08-03 14:24:48 +0700

    mars - done option export all module po

--- export_all_po/models/models.py
+++ export_all_po/models/models.py
@@ -2,14 +2,15 @@
 
 from odoo import models, fields, api
 
-# class export_all_po(models.Model):
-#     _name = 'export_all_po.export_all_po'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class ExportPO(models.TransientModel):
+    _inherit = 'base.language.export'
+
+    export_all = fields.Boolean('Export all modules', default=False)
+
+    @api.onchange('export_all')
+    def _onchange_export_all(self):
+        if self.export_all:
+            self.modules = self.env['ir.module.module'].search([('state', '=', 'installed')]).ids
+        else:
+            self.modules = None
