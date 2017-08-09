a2d08ae4f5c92060a3c66a86fbde76d77774fb4b
holmesnguyen <holmes@trueplus.vn>
2017-08-09 09:22:14 +0700

    Holmes - change kanban color

---
+++ change_kanban_color/models/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class change_kanban_color(models.Model):
+    _inherit = "project.task.type"
+    color = fields.Integer(string="Color", store=True,help="Choose a color index, from 0 to 9")
+
+class change_kanban(models.Model):
+    _inherit = "project.task"
+
+    color = fields.Integer(string='Color Index', compute='color_compute', )
+
+    @api.depends("stage_id")
+    def color_compute(self):
+        for item in self:
+            stage_id = item.stage_id.id
+            if item.color == 0:
+                item.color = item.env['project.task.type'].search([('id', '=',stage_id)]).color
+
+
+
+
+
