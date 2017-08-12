7268bf38fee9404bfaf2f9766f4c6ac807da7a45
holmesnguyen <holmes@trueplus.vn>
2017-08-12 17:13:07 +0700

    Holmes - Issue add member to note fixed.

--- add_member_to_note/models/models.py
+++ add_member_to_note/models/models.py
@@ -16,3 +16,23 @@ class add_member_to_note(models.Model):
         note = super(add_member_to_note, self).create(vals)
         return note
 
+class add_member(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def create(self, vals):
+        user = super(add_member, self).create(vals)
+
+        for item in self.env['note.note'].search([]):
+            self.add_follower_id(item.id, user.id, "note.note")
+        return user
+
+    def add_follower_id(self, res_id, partner_id, model):
+        followers_obj = self.env['mail.followers']
+        reg = {
+            'res_id': res_id,
+            'res_model': model,
+            'partner_id': partner_id, }
+        follower_id = followers_obj.create(reg)
+        return follower_id
+
